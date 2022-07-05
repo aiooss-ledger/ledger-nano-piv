cfb60abd180caa25cec60bd1c01b99c5041df8eb
abonnaudet-ledger <arthur.bonnaudet@ledger.fr>
2022-07-05 11:37:13 +0200

    Implement response continue mechanism

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,83 @@ nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
 /// Status Words as specified in table 6 of Interfaces for Personal Identity
 /// Verification specification.
 #[derive(Copy, Clone)]
-#[repr(u16)]
+// #[repr(u16)]
 enum StatusWords {
+    MoreDataAvailable(u8),
     // VerificationFailed = 0x6300,
-    WrongLength = 0x6700,
+    WrongLength,
     // SecureMessagingNotSupported = 0x6882,
     // SecurityStatusNotSatisfied = 0x6982,
     // AuthMethodBlocked = 0x6983,
     // MissingSecureMessagingData = 0x6987,
     // IncorrectSecureMessagingData = 0x6988,
-    WrongData = 0x6A80,
-    FuncNotSupported = 0x6A81,
-    FileNotFound = 0x6A82,
+    WrongData,
+    FuncNotSupported,
+    FileNotFound,
     // FileFull = 0x6A84,
-    IncorrectP1P2 = 0x6A86,
+    IncorrectP1P2,
     // RefDataNotFound = 0x6A88,
+}
+
+impl From<StatusWords> for u16 {
+    fn from(val: StatusWords) -> Self {
+        match val {
+            StatusWords::MoreDataAvailable(size) => 0x6100 + (size as u16),
+            StatusWords::WrongLength => 0x6700,
+            StatusWords::WrongData => 0x6A80,
+            StatusWords::FuncNotSupported => 0x6A81,
+            StatusWords::FileNotFound => 0x6A82,
+            StatusWords::IncorrectP1P2 => 0x6A86,
+        }
+    }
 }
 
 impl From<StatusWords> for io::Reply {
     fn from(sw: StatusWords) -> io::Reply {
-        io::Reply(sw as u16)
+        io::Reply(sw.into())
     }
 }
 
 const PIV_APP_AID: [u8; 5] = [0xa0, 0x00, 0x00, 0x03, 0x08];
+
+const APDU_MAX_CHUNK_SIZE: usize = 255;
+const DATA_RESP_BUFFER_SIZE: usize = 512;
+
+struct DataResponseBuffer {
+    data: [u8; DATA_RESP_BUFFER_SIZE],
+    read_cnt: usize,
+}
+
+impl DataResponseBuffer {
+    fn new() -> Self {
+        Self {
+            data: [0; DATA_RESP_BUFFER_SIZE],
+            read_cnt: 0,
+        }
+    }
+
+    fn remaining_length(&self) -> usize {
+        DATA_RESP_BUFFER_SIZE - self.read_cnt
+    }
+
+    fn set(&mut self, data: &[u8]) {
+        let copied_length = data.len().min(DATA_RESP_BUFFER_SIZE);
+        self.data.copy_from_slice(&data[0..copied_length]);
+        self.read_cnt = 0;
+    }
+
+    fn get_next_chunk_size(&self) -> usize {
+        APDU_MAX_CHUNK_SIZE.min(self.remaining_length())
+    }
+
+    fn read_next_chunk(&mut self) -> &[u8] {
+        let read_length = self.get_next_chunk_size();
+        let begin = self.read_cnt;
+        let end = self.read_cnt + read_length;
+        self.read_cnt += read_length;
+        &self.data[begin..end]
+    }
+}
 
 /// Select card command
 fn process_select_card(comm: &mut io::Comm) {
@@ -66,18 +119,77 @@ fn process_general_auth(comm: &mut io::Comm) {
     comm.reply_ok();
 }
 
+fn compute_continue_response(comm: &mut io::Comm, data_response_buffer: &mut DataResponseBuffer) {
+    if data_response_buffer.get_next_chunk_size() == 0 {
+        // No data to respond
+        comm.reply_ok();
+        return;
+    }
+
+    // Read data
+    comm.append(data_response_buffer.read_next_chunk());
+
+    // Reply status
+    let next_size = data_response_buffer.get_next_chunk_size();
+    if next_size > 0 {
+        comm.reply(StatusWords::MoreDataAvailable(next_size as u8));
+    } else {
+        comm.reply_ok();
+    }
+}
+
 /// Ask the card to continue to answer
-fn continue_response(comm: &mut io::Comm) {
+fn process_continue_response(comm: &mut io::Comm, data_response_buffer: &mut DataResponseBuffer) {
     if comm.get_p1() != 0x00 || comm.get_p2() != 0x00 {
         return comm.reply(StatusWords::IncorrectP1P2);
     }
 
-    // TODO
-    comm.reply_ok();
+    compute_continue_response(comm, data_response_buffer);
 }
 
-// Process get data
-fn process_get_data(comm: &mut io::Comm) {
+/// Check 'get data' command parameters
+fn check_get_data_params(data: &[u8]) -> Option<StatusWords> {
+    let tag = data[0];
+    let len = data[1];
+
+    // Check tag
+    if tag != 0x5C {
+        return Some(StatusWords::WrongData);
+    }
+
+    // Check length
+    if len as usize != data.len() - 2 {
+        return Some(StatusWords::WrongLength);
+    }
+
+    // Extract slot number
+    if data[2] != 0x5F {
+        return Some(StatusWords::FileNotFound);
+    }
+
+    if data[3] != 0xC1 {
+        // Todo: handle yk files
+        // https://github.com/arekinath/PivApplet/blob/60fc61ac21fda3caf6cbd4c96f7a7e2db07f2a32/src/net/cooperi/pivapplet/PivApplet.java#L2849
+        return Some(StatusWords::FileNotFound);
+    }
+
+    if data[4] != 0x0D {
+        // Todo: handle multiple slots
+        return Some(StatusWords::FileNotFound);
+    }
+
+    None
+}
+
+fn compute_get_data_content(data_response_buffer: &mut DataResponseBuffer) {
+    // Compute data buffer: todo
+    let hardcoded = [0xA5; 512];
+
+    // Set data response buffer
+    data_response_buffer.set(&hardcoded);
+}
+
+fn process_get_data(comm: &mut io::Comm, data_response_buffer: &mut DataResponseBuffer) {
     if comm.get_p1() != 0x3F || comm.get_p2() != 0xFF {
         return comm.reply(StatusWords::IncorrectP1P2);
     }
@@ -89,37 +201,16 @@ fn process_get_data(comm: &mut io::Comm) {
         }
     };
 
-    let tag = data[0];
-    let len = data[1];
-
-    // Check tag
-    if tag != 0x5C {
-        return comm.reply(StatusWords::WrongData);
+    // Check params
+    if let Some(status) = check_get_data_params(data) {
+        return comm.reply(status);
     }
 
-    // Check length
-    if len as usize != data.len() - 2 {
-        return comm.reply(StatusWords::WrongLength);
-    }
+    // Compute get data content
+    compute_get_data_content(data_response_buffer);
 
-    // Extract slot number
-    if data[2] != 0x5F {
-        return comm.reply(StatusWords::FileNotFound);
-    }
-
-    if data[3] != 0xC1 {
-        // Todo: handle yk files
-        // https://github.com/arekinath/PivApplet/blob/60fc61ac21fda3caf6cbd4c96f7a7e2db07f2a32/src/net/cooperi/pivapplet/PivApplet.java#L2849
-        return comm.reply(StatusWords::FileNotFound);
-    }
-
-    if data[4] != 0x0D {
-        // Todo: handle multiple slots
-        return comm.reply(StatusWords::FileNotFound);
-    }
-
-    // Todo: replay data
-    comm.reply_ok();
+    // Response
+    compute_continue_response(comm, data_response_buffer);
 }
 
 /// Get ledger serial
@@ -167,6 +258,9 @@ extern "C" fn sample_main() {
     screen_util::fulldraw(0, 0, &bitmaps::BLANK);
     bitmaps::PADLOCK.draw(64 - (bitmaps::PADLOCK.width as i32) / 2, 4);
     "*PIV* app".display(Line::Second, Layout::Centered);
+
+    let mut data_response_buffer = DataResponseBuffer::new();
+
     loop {
         match comm.next_event() {
             io::Event::Button(ButtonEvent::BothButtonsRelease) => nanos_sdk::exit_app(0),
@@ -176,8 +270,10 @@ extern "C" fn sample_main() {
             // See https://csrc.nist.gov/publications/detail/sp/800-73/4/final
             io::Event::Command(0xA4) => process_select_card(&mut comm),
             io::Event::Command(0x87) => process_general_auth(&mut comm),
-            io::Event::Command(0xC0) => continue_response(&mut comm),
-            io::Event::Command(0xCB) => process_get_data(&mut comm),
+            io::Event::Command(0xC0) => {
+                process_continue_response(&mut comm, &mut data_response_buffer)
+            }
+            io::Event::Command(0xCB) => process_get_data(&mut comm, &mut data_response_buffer),
 
             // YubicoPIV extensions
             // See https://developers.yubico.com/PIV/Introduction/Yubico_extensions.html
